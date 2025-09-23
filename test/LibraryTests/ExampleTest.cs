0b682ac8090d850921a77024c6a6d7e9824afdcf
facundon.rodriguez@correo.ucu.edu.uy <facundon.rodriguez@correo.ucu.edu.uy>
2025-09-23 09:29:11 -0300

    tests

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -1,18 +1,79 @@
-using Library;
-
-namespace LibraryTests;
-
-public class Tests
+namespace Library.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class EnanosTests
     {
-    }
+        [Test]
+        public void Constructor_CreaEnanoConVida100YDosItems()
+        {
+            Enanos enano = new Enanos("Gimli");
 
-    [Test]
-    public void CrearElfoTest(Elfos elfo)
-    {
-        Elfos Adam = new Elfos("Adam");
-        Assert.Pass();
+            Assert.AreEqual("Gimli", enano.Nombre);
+            Assert.AreEqual(100, enano.Vida);
+            Assert.AreEqual(2, enano.Inventario.Count);
+            Assert.AreEqual("Espada", enano.Inventario[0].Nombre);
+            Assert.AreEqual("Escudo", enano.Inventario[1].Nombre);
+        }
+
+        [Test]
+        public void ObtenerAtaqueTotal_SumaAtaquesDeItems()
+        {
+            Enanos enano = new Enanos("Gimli");
+
+            int ataqueTotal = enano.ObtenerAtaqueTotal();
+
+            Assert.AreEqual(100, ataqueTotal);
+        }
+
+        [Test]
+        public void ObtenerDefensaTotal_SumaDefensasDeItems()
+        {
+            Enanos enano = new Enanos("Gimli");
+
+            int defensaTotal = enano.ObtenerDefensaTotal();
+
+            Assert.AreEqual(50, defensaTotal);
+        }
+
+        [Test]
+        public void RecibirDaño_MayorQueDefensa_RestaVidaCorrectamente()
+        {
+            Enanos enano = new Enanos("Gimli");
+
+            enano.RecibirDaño(80); // 80 - defensa 50 = 30
+
+            Assert.AreEqual(70, enano.Vida);
+        }
+
+        [Test]
+        public void RecibirDaño_MenorQueDefensa_NoRestaVida()
+        {
+            Enanos enano = new Enanos("Gimli");
+
+            enano.RecibirDaño(40); // 40 - defensa 50 = daño real 0
+
+            Assert.AreEqual(100, enano.Vida);
+        }
+
+        [Test]
+        public void RecibirDaño_NoPermiteVidaNegativa()
+        {
+            Enanos enano = new Enanos("Gimli");
+
+            enano.RecibirDaño(500);
+
+            Assert.AreEqual(0, enano.Vida);
+        }
+
+        [Test]
+        public void Atacar_RestaVidaDelEnemigo()
+        {
+            Enanos atacante = new Enanos("Gimli");
+            Enanos defensor = new Enanos("Thorin");
+
+            atacante.Atacar(defensor);
+
+            // ataque = 100, defensa = 50, daño real = 50
+            Assert.AreEqual(50, defensor.Vida);
+        }
     }
 }
