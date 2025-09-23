c5e61801f9f774a4abf68c2ae045c0915e7f966a
cfacu23 <77148254+cfacu23@users.noreply.github.com>
2025-09-23 09:48:44 -0300

    Corrección del codigo de test

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -61,9 +61,6 @@ namespace LibraryTests
         }
     }
 
-    // -------------------------------
-    // TESTS DE ENANOS
-    // -------------------------------
     public class EnanosTests
     {
         [Test]
